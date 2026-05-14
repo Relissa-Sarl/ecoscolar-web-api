39f3dcdbb6ba24c88583da6515f4dacd6e6aad28
Alexis1476 <halexisr20@hotmail.com>
2026-05-14 11:59:02 +0200

    feat: add swagger to see current endpoints

--- EcoScolarWebApi/Program.cs
+++ EcoScolarWebApi/Program.cs
@@ -32,6 +32,7 @@ namespace EcoscolarWebApi
             // Add services to the container.
             builder.Services.AddControllers();
             builder.Services.AddOpenApi();
+            builder.Services.AddSwaggerGen();
 
             // Setup CORS policy
             builder.Services.AddCors(options =>
@@ -53,7 +54,14 @@ namespace EcoscolarWebApi
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
+            {
                 app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "EcoScolar Web API V1");
+                });
+            }
 
             app.UseHttpsRedirection();
             app.UseAuthentication();
