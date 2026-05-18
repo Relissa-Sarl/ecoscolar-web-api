8a4ac9838d4b16e781a9298139fd69d9aa465cf0
Alexis1476 <halexisr20@hotmail.com>
2026-05-18 20:53:25 +0200

    feat(favorites): create entity UserFavorite to handle user favorites

---
+++ EcoScolarWebApi/Migrations/20260518181528_AddUserFavorites.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcoscolarWebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserFavorites : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PhysicalItems_ProductCategories_ProductCategoryId",
+                table: "PhysicalItems");
+
+            migrationBuilder.CreateTable(
+                name: "UserFavorites",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    AdvertId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserFavorites", x => new { x.UserId, x.AdvertId });
+                    table.ForeignKey(
+                        name: "FK_UserFavorites_Adverts_AdvertId",
+                        column: x => x.AdvertId,
+                        principalTable: "Adverts",
+                        principalColumn: "AdvertId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserFavorites_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserFavorites_AdvertId",
+                table: "UserFavorites",
+                column: "AdvertId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PhysicalItems_ProductCategories_ProductCategoryId",
+                table: "PhysicalItems",
+                column: "ProductCategoryId",
+                principalTable: "ProductCategories",
+                principalColumn: "ProductCategoryId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PhysicalItems_ProductCategories_ProductCategoryId",
+                table: "PhysicalItems");
+
+            migrationBuilder.DropTable(
+                name: "UserFavorites");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PhysicalItems_ProductCategories_ProductCategoryId",
+                table: "PhysicalItems",
+                column: "ProductCategoryId",
+                principalTable: "ProductCategories",
+                principalColumn: "ProductCategoryId");
+        }
+    }
+}
---
+++ EcoScolarWebApi/Models/UserFavorite.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcoscolarWebApi.Models;
+
+[Table("UserFavorites")]
+public class UserFavorite
+{
+	[Required]
+	public required string UserId { get; set; }
+
+	[ForeignKey("UserId")]
+	public virtual User? User { get; set; }
+
+	[Required]
+	public long AdvertId { get; set; }
+
+	[ForeignKey("AdvertId")]
+	public virtual Adverts? Advert { get; set; }
+}
