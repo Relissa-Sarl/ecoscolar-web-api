43be09d68a3269edb7f414b7d39b2d7807ac7999
flavio <211345279+Marrii00@users.noreply.github.com>
2026-05-29 10:39:42 +0200

    feat: register UserSearchAlert in DbContext

--- EcoScolarWebApi/Data/EcoscolarDbContext.cs
+++ EcoScolarWebApi/Data/EcoscolarDbContext.cs
@@ -21,6 +21,7 @@ public class EcoscolarDbContext(DbContextOptions<EcoscolarDbContext> options) : 
     public DbSet<Picture> Pictures { get; set; } = default!;
     public DbSet<ProductCategory> ProductCategories { get; set; } = default!;
     public DbSet<UserFavorite> UserFavorites { get; set; } = default!;
+    public DbSet<UserSearchAlert> UserSearchAlerts { get; set; } = default!;
     public DbSet<SchoolGrade> SchoolGrades { get; set; } = default!;
     public DbSet<Subject> Subjects { get; set; } = default!;
     public DbSet<BookCategory> BookCategories { get; set; } = default!;
@@ -51,6 +52,13 @@ public class EcoscolarDbContext(DbContextOptions<EcoscolarDbContext> options) : 
             .HasOne(uf => uf.Advert)
             .WithMany()
             .HasForeignKey(uf => uf.AdvertId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        // User Search Alerts
+        builder.Entity<UserSearchAlert>()
+            .HasOne(a => a.User)
+            .WithMany()
+            .HasForeignKey(a => a.UserId)
             .OnDelete(DeleteBehavior.Cascade);
 
         // Dispute
