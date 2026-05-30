f970d6860d1741579664f980bd17ac6f4cae6b5d
Alexis1476 <halexisr20@hotmail.com>
2026-05-30 18:32:52 +0200

    feat: add DTOs and mapper for advert public comments (advert questions)

---
+++ EcoScolarWebApi/Mappers/PublicCommentMapper.cs
@@ -0,0 +1,18 @@
+﻿using EcoScolarWebApi.DTOs.Adverts;
+using EcoScolarWebApi.Models;
+using Riok.Mapperly.Abstractions;
+
+namespace EcoScolarWebApi.Mappers;
+
+[Mapper]
+public partial class PublicCommentMapper
+{
+	[MapProperty(nameof(PublicComment.AuthorId), nameof(QuestionResponseDTO.AuthorId))]
+	[MapProperty("Author.UserName", nameof(QuestionResponseDTO.Author))]
+	public partial QuestionResponseDTO ToQuestionResponse(PublicComment comment);
+
+	[MapProperty("Advert.Seller.UserName", nameof(AnswerResponseDTO.Seller))]
+	[MapProperty(nameof(PublicComment.Answer), nameof(AnswerResponseDTO.Answer))]
+	[MapProperty(nameof(PublicComment.AnsweredAt), nameof(AnswerResponseDTO.AnsweredAt))]
+	public partial AnswerResponseDTO ToAnswerResponse(PublicComment comment);
+}
