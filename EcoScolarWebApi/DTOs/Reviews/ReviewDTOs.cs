7d29105cedba4701698c042b0223b4014024b720
Alexis1476 <halexisr20@hotmail.com>
2026-05-31 21:26:25 +0200

    feat: add reviewed and reviewer nicknames in reviewDTOs

--- EcoScolarWebApi/DTOs/Reviews/ReviewDTOs.cs
+++ EcoScolarWebApi/DTOs/Reviews/ReviewDTOs.cs
@@ -2,6 +2,18 @@ using System.ComponentModel.DataAnnotations;
 
 namespace EcoScolarWebApi.DTOs.Reviews;
 
+// DTO for creating a new review, with validation attributes for rating
 public record ReviewRequestDTO([Required][Range(1, 5)] int Rating, string? Comment);
 
-public record ReviewResponseDTO(int ReviewId, string? Comment, int Rating, DateTime Date, string ReviewerId, string ReviewedId, long TransactionId);
+// DTO for returning review details, including reviewer and reviewed user information
+public record ReviewResponseDTO(
+	int ReviewId,
+	string? Comment,
+	int Rating,
+	DateTime Date,
+	string ReviewerId,
+	string ReviewerNickname,
+	string ReviewedId,
+	string ReviewedNickname,
+	long TransactionId
+	);
--- EcoScolarWebApi/Mappers/ReviewMapper.cs
+++ EcoScolarWebApi/Mappers/ReviewMapper.cs
@@ -1,3 +1,4 @@
+using EcoScolarWebApi.DTOs.Adverts;
 using EcoScolarWebApi.DTOs.Reviews;
 using EcoScolarWebApi.Models;
 using Riok.Mapperly.Abstractions;
@@ -7,5 +8,11 @@ namespace EcoScolarWebApi.Mappers;
 [Mapper]
 public partial class ReviewMapper
 {
+	[MapProperty("Reviewed.Nickname", nameof(ReviewResponseDTO.ReviewedNickname))]
+	[MapProperty("Reviewer.Nickname", nameof(ReviewResponseDTO.ReviewerNickname))]
 	public partial ReviewResponseDTO ToReviewResponseDTO(Review review);
+	public partial IEnumerable<ReviewResponseDTO> ToReviewsResponseDTO(IEnumerable<Review> reviews);
+	[MapProperty("Reviewed.Nickname", nameof(ReviewResponseDTO.ReviewedNickname))]
+	[MapProperty("Reviewer.Nickname", nameof(ReviewResponseDTO.ReviewerNickname))]
+	public partial IQueryable<ReviewResponseDTO> ProjectToReviewResponseDTOs(IQueryable<Review> reviews);
 }
