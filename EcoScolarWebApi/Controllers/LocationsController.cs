d4ee175a269f5a449f72a09bca1bd67ae4787441
Alexis1476 <halexisr20@hotmail.com>
2026-06-05 16:22:15 +0200

    feat: add endpoint to search localities

---
+++ EcoScolarWebApi/Controllers/LocationsController.cs
@@ -0,0 +1,42 @@
+﻿using Asp.Versioning;
+using EcoScolarWebApi.Data;
+using EcoScolarWebApi.DTOs.ReferenceData;
+using EcoScolarWebApi.Mappers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcoScolarWebApi.Controllers;
+
+[ApiVersion("1.0")]
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiController]
+public class LocationsController : ControllerBase
+{
+	private readonly EcoscolarDbContext _context;
+	private readonly LocationMapper _mapper;
+
+	public LocationsController(EcoscolarDbContext context)
+	{
+		_context = context;
+		_mapper = new LocationMapper();
+	}
+
+	[HttpGet("search")]
+	public async Task<ActionResult<IEnumerable<LocationResponseDto>>> SearchLocations([FromQuery] string query)
+	{
+		// Return an empty list if the query is null, empty, or too short to prevent unnecessary database queries
+		if (string.IsNullOrWhiteSpace(query) || query.Length < 2)
+			return Ok(Enumerable.Empty<LocationResponseDto>());
+
+		var searchTerm = query.Trim().ToLower();
+
+		var efQuery = _context.Locations
+			.Where(l => l.PostalCode.StartsWith(searchTerm) || l.City.ToLower().Contains(searchTerm))
+			.OrderBy(l => l.PostalCode)
+			.Take(15);
+
+		var locations = await _mapper.ProjectToLocationResponseDto(efQuery).ToListAsync();
+
+		return Ok(locations);
+	}
+}
---
+++ EcoScolarWebApi/Mappers/LocationMapper.cs
@@ -0,0 +1,12 @@
+﻿using EcoScolarWebApi.DTOs.ReferenceData;
+using EcoScolarWebApi.Models;
+using Riok.Mapperly.Abstractions;
+
+namespace EcoScolarWebApi.Mappers;
+
+[Mapper]
+public partial class LocationMapper
+{
+	public partial LocationResponseDto LocationToLocationResponseDto(Location location);
+	public partial IQueryable<LocationResponseDto> ProjectToLocationResponseDto(IQueryable<Location> query);
+}
