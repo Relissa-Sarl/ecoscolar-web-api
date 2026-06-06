e8e5aea1ed72476c8f690f8f9f23f6f609a89234
Thomas Rey <pro.thomas.rey@gmail.com>
2026-06-06 18:20:12 +0200

    feat: add ban toggle for admin

--- EcoScolarWebApi/Services/UserService.cs
+++ EcoScolarWebApi/Services/UserService.cs
@@ -131,6 +131,11 @@ public class UserService : IUserService
 		return Result<UserPublicReadDto>.Success(UserPublicReadDto.FromEntity(user));
 	}
 
+	/// <summary>
+	/// Get all users if the connected user is admin
+	/// </summary>
+	/// <param name="user">Connected user principal</param>
+	/// <returns>A Result object with a UserResponse DTO value; otherwise, a failure result indicating the reason.</returns>
 	public async Task<Result<List<UserResponse>>> GetAllUsers(ClaimsPrincipal user)
 	{
         if (!user.IsInRole("Admin"))
@@ -146,6 +151,42 @@ public class UserService : IUserService
         }
         return Result<List<UserResponse>>.Success(userDtos);
 	}
+
+	public async Task<Result<UserResponse>> BanUserToggle(ClaimsPrincipal user, string userId)
+	{
+        var currentUser = await _userManager.Users
+            .Include(u => u.Languages)
+            .FirstOrDefaultAsync(u => u.Id == userId);
+        if (currentUser == null)
+            return Result<UserResponse>.Failure("User not found.", ErrorType.NotFound);
+
+		if (!user.IsInRole("Admin") || _userManager.IsInRoleAsync(currentUser, "Admin").Result)
+            return Result<UserResponse>.Failure("Unauthorized access.", ErrorType.Unauthorized);
+
+        currentUser.IsBanned = !currentUser.IsBanned;
+		if (currentUser.IsBanned)
+		{
+            //await _userManager.SetLockoutEnabledAsync(currentUser, true);
+            await _userManager.SetLockoutEndDateAsync(currentUser, DateTime.Today.AddYears(999));
+		}
+		else
+		{
+            //await _userManager.SetLockoutEnabledAsync(currentUser, false);
+            await _userManager.SetLockoutEndDateAsync(currentUser, null);
+        }
+
+        var updateResult = await _userManager.UpdateAsync(currentUser);
+        if (!updateResult.Succeeded)
+        {
+            var errors = updateResult.Errors.Select(e => e.Description);
+            return Result<UserResponse>.Failure(errors);
+        }
+
+        var roles = await _userManager.GetRolesAsync(currentUser);
+
+        return Result<UserResponse>.Success(_userMapper.ToResponse(currentUser) with { Roles = roles.ToArray() });
+    }
+
     /// <summary>
     ///	Anonymize the currently connected user profile when deleting his account
     /// </summary>
